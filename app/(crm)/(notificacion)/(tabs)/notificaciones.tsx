e42de86edfb56e7dbd5c7b3b52d2acca2abb2ece
Mario-Vargas-Eco <luismariovargaspalma@gmail.com>
2024-10-30 21:32:00 -0600

    Update notificaciones.tsx

--- app/(crm)/(notificacion)/(tabs)/notificaciones.tsx
+++ app/(crm)/(notificacion)/(tabs)/notificaciones.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
-import { Text, View, ScrollView, ActivityIndicator } from "react-native";
+import React, { useEffect } from 'react'; 
+import { Text, View, ScrollView, ActivityIndicator, TouchableOpacity } from "react-native";
 import useNotificaciones from "@/hooks/useNotificaciones";
 import { Notificacion } from "@/models/Notificacion"; 
+import { StyleSheet } from "react-native";
 
 const Notificaciones = () => {
-  const { notificaciones, cargando, getNotificaciones } = useNotificaciones();
+  const { notificaciones, cargando, getNotificaciones, marcarVisto } = useNotificaciones();
 
   // Llama a getNotificaciones cuando el componente se monte
   useEffect(() => {
@@ -22,11 +23,15 @@ const Notificaciones = () => {
       ) : (
         notificaciones?.length ? (
           notificaciones.map((notificacion: Notificacion) => (
-            <View key={notificacion.id} style={{ padding: 10, borderBottomWidth: 1 }}>
-              <Text>{notificacion.mensaje}</Text>
-              <Text>{new Date(notificacion.fecha).toLocaleString()}</Text>
-              <Text>{notificacion.visto ? "Visto" : "No visto"}</Text>
-            </View>
+            <TouchableOpacity 
+              key={notificacion.id}
+              onPress={() => marcarVisto(notificacion.id)}
+            >
+              <View style={notificacion.visto ? styles.moduleCard : styles.moduleCardNotSeen }>
+                <Text>{notificacion.mensaje}</Text>
+                <Text>{new Date(notificacion.fecha).toLocaleString()}</Text>
+              </View>
+            </TouchableOpacity>
           ))
         ) : (
           <Text style={{ padding: 10 }}>No hay notificaciones</Text>
@@ -35,5 +40,110 @@ const Notificaciones = () => {
     </ScrollView>
   );
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  headerIcons: {
+    flexDirection: "row",
+  },
+  userInfo: {
+    backgroundColor: "#fff",
+    padding: 20,
+    marginBottom: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+    flexDirection: "row",
+    marginVertical: 20,
+  },
+  circle: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#007bff", // Cambia este color según tu diseño
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 15,
+  },
+  initial: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  modulesGrid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  moduleCard: {
+    backgroundColor: "#fff",
+    width: "98%", // Dos columnas
+    padding: 20,
+    marginBottom: 15,
+    borderRadius: 10,
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+    moduleCardNotSeen: {
+    backgroundColor: "orange",
+    width: "98%", // Dos columnas
+    padding: 20,
+    marginBottom: 15,
+    borderRadius: 10,
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  moduleName: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  logoutButton: {
+    marginTop: 20,
+    paddingVertical: 12,
+    backgroundColor: "#212121", // Color rojo para el botón
+    borderRadius: 5,
+    alignItems: "center",
+    marginBottom: 60,
+  },
+  logoutButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
 
 export default Notificaciones;
