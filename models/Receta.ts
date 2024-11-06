e24d9fba5586c86a6afeb01083db485e9134f060
IDGS-802-21000151 <jesusluviancontacto@gmail.com>
2024-11-06 17:34:41 -0600

    Finalizado

--- models/Receta.ts
+++ models/Receta.ts
@@ -21,24 +21,9 @@ type Receta = {
   tiempoVida: number;
   activo: boolean;
   fechaRegistrado: string;
-};
-export interface Receta {
-  id: number;
-  nombre: string;
-  especificaciones: string;
-  precioPaquete1: number;
-  precioPaquete6: number;
-  precioPaquete12: number;
-  precioPaquete24: number;
-  puntuacion: number;
-  lotesMinimos: number;
-  lotesMaximos: number;
-  imagen: string;
-  rutaFondo: string;
   fecha_registrado: string;
   favorito: boolean;
   nuevo: boolean;
   cantidadEnStock: number;
   estaEnCarrito: boolean;
-  descripcion: string;
-}
+};
