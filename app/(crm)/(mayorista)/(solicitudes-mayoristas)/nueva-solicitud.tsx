ff99123e19dd2fa88db2655851b3fce1c90da532
CarlosIRB <ramirezbelloci@gmail.com>
2024-11-25 01:10:04 -0600

    Borrar botones sin uso menu

--- app/(crm)/(mayorista)/(solicitudes-mayoristas)/nueva-solicitud.tsx
+++ app/(crm)/(mayorista)/(solicitudes-mayoristas)/nueva-solicitud.tsx
@@ -35,6 +35,8 @@ export default function NuevaSolicitud() {
   useEffect(() => {
     getRecetas();
     obtenerProductosCarrito();
+    console.log("recetas", recetas);
+    console.log("productosCarrito", productosCarrito);
   }, []);
 
   return (
